c6cabf50fb7f832fc1a05f15f7730dadc8bb79f4
mk <v.a.stekolschikov@gmail.com>
2022-01-10 14:27:41 +0200

    temp

--- src/App.js
+++ src/App.js
@@ -1,65 +1,20 @@
-import './App.css';
-import { useState, useRef } from 'react'
-import { useRect } from '@reactour/utils'
-import { Mask } from '@reactour/mask'
+import React, { useState } from "react";
+
+import Demo from "./Demo";
+import { useTour } from "@reactour/tour";
+import "./styles.css";
 
 function App() {
+  const { setIsOpen } = useTour();
+  const [isShowingMore, setIsShowingMore] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false)
-    const [isOpen2, setIsOpen2] = useState(false)
-    const ref = useRef(null)
-    const ref2 = useRef(null)
-    const sizes = useRect(ref)
-    const sizes2 = useRect(ref2)
-
-    return (
-
-        <div className="App">
-            <ul>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li ref={ref2}>1</li>
-                <li>1</li>
-                <li>1</li>
-            </ul>
-            <button onClick={() => setIsOpen2(o => !o)}>
-                {isOpen2 ? 'Close' : 'Open'} Mask 2
-            </button>
-            <button onClick={() => setIsOpen(o => !o)} ref={ref}>
-                {isOpen ? 'Close' : 'Open'} Mask
-            </button>
-            {isOpen ? (
-                <>
-                    <Mask
-                        sizes={sizes}
-                        onClick={() => setIsOpen(false)}
-                        styles={{
-                            // maskWrapper: base => ({ ...base, color, opacity: 0.99 }),
-                            // maskArea: base => ({ ...base, rx: 10 }),
-                        }}
-                    />
-                </>
-            ) : null}
-            {isOpen2 ? (
-                <>
-                    <h1>22</h1>
-                    <Mask
-                        sizes={sizes2}
-                        onClick={() => setIsOpen2(false)}
-                        styles={{
-                            maskWrapper: base => ({ ...base, opacity: 0.5 }),
-                            maskArea: base => ({ ...base, rx: 10 }),
-                        }}
-                    />
-                </>
-            ) : null}
-        </div>
-    );
+  return (
+    <Demo
+      openTour={() => setIsOpen(true)}
+      toggleShowMore={() => setIsShowingMore((m) => !m)}
+      isShowingMore={isShowingMore}
+    />
+  );
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {TourProvider} from '@reactour/tour'
+import React from "react";
+import { render } from "react-dom";
+import { TourProvider } from "@reactour/tour";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
+import App from "./App";
+import steps from "./steps";
 
-const steps = [
-    {
-        selector: '.first-step',
-        content: 'This is my first Step',
-    },
-    {
-        selector: '#test2',
-        content: 'This is my first Step',
-    },
-    // ...
-]
+function MainApp() {
+  const disableBody = (target) => disableBodyScroll(target);
+  const enableBody = (target) => enableBodyScroll(target);
+  return (
+    <TourProvider
+      steps={steps}
+      afterOpen={disableBody}
+      beforeClose={enableBody}
+      showBadge={false}
+      close={"disabled"}
+>
+      <App />
+    </TourProvider>
+  );
+}
 
-ReactDOM.render(
-    <React.StrictMode>
-        <TourProvider steps={steps}>
-
-            <App/>
-            {/*<h3>An SVG mask layer (formed as a triangle):</h3>*/}
-
-
-            {/*<h3>Original image:</h3>*/}
-            {/*<img src="https://www.w3schools.com/css/img_5terre.jpg" alt="Cinque Terre" width="600" height="400"/>*/}
-
-        </TourProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(<MainApp />, document.getElementById("root"));
