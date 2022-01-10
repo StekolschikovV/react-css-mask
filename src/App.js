e76332a97542db3b18517acdbc98eb525f556e76
mk <v.a.stekolschikov@gmail.com>
2022-01-10 11:38:48 +0200

    temp

--- src/App.js
+++ src/App.js
@@ -1,62 +1,100 @@
 import './App.css';
-import { useState, useRef } from 'react'
-import { useRect } from '@reactour/utils'
-import { Mask } from '@reactour/mask'
+import {useEffect, useRef, useState} from 'react'
+import {useRect} from '@reactour/utils'
+import {Mask} from '@reactour/mask'
+import {AnimatePresence, motion} from 'framer-motion'
+// import {useIntersectionObserver} from './hooks.js'
+import {Popover} from '@reactour/popover'
 
 function App() {
 
     const [isOpen, setIsOpen] = useState(false)
-    const [isOpen2, setIsOpen2] = useState(false)
+    const [updater, setUpdater] = useState([])
     const ref = useRef(null)
     const ref2 = useRef(null)
-    const sizes = useRect(ref)
-    const sizes2 = useRect(ref2)
+    const sizes = useRect(ref2, updater)
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            setUpdater([])
+        })
+        return () => {
+            window.removeEventListener('scroll', () => {
+                setUpdater([])
+            })
+        }
+    }, [setUpdater])
+
+    const wrapperRef = useRef(null)
+    // const entry = useIntersectionObserver(ref, {})
+    // const isVisible = entry && !!entry.isIntersecting
 
     return (
 
         <div className="App">
-            <ul>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li>1</li>
-                <li ref={ref2}>1</li>
-                <li>1</li>
-                <li>1</li>
-            </ul>
-            <button onClick={() => setIsOpen2(o => !o)}>
-                {isOpen2 ? 'Close' : 'Open'} Mask 2
-            </button>
-            <button onClick={() => setIsOpen(o => !o)} ref={ref}>
-                {isOpen ? 'Close' : 'Open'} Mask
-            </button>
-            {isOpen ? (
-                <>
-                    <Mask
-                        sizes={sizes}
-                        onClick={() => setIsOpen(false)}
-                        styles={{
-                            // maskWrapper: base => ({ ...base, color, opacity: 0.99 }),
-                            // maskArea: base => ({ ...base, rx: 10 }),
-                        }}
-                    />
-                </>
-            ) : null}
-            {isOpen2 ? (
-                <>
-                    <h1>22</h1>
-                    <Mask
-                        sizes={sizes2}
-                        onClick={() => setIsOpen2(false)}
-                        styles={{
-                            maskWrapper: base => ({ ...base, opacity: 0.5 }),
-                            maskArea: base => ({ ...base, rx: 10 }),
-                        }}
-                    />
-                </>
-            ) : null}
+            ;
+            <div ref={wrapperRef}>
+                <button onClick={() => setIsOpen(o => !o)}>
+                    {isOpen ? 'Hide' : 'Show'} Popover
+                </button>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus volutpat
+                    quam eu mauris euismod imperdiet. Nullam elementum fermentum neque a
+                    placerat. Vivamus sed dui nisi. Phasellus vel dolor interdum, accumsan eros
+                    ut, rutrum dolor. Etiam in leo urna. Vestibulum maximus vitae urna at
+                    congue. Vivamus lectus nisi, pellentesque at orci a, tempor lobortis orci.{' '}
+                    <span ref={ref} style={{color: '#5ae'}}>
+      Praesent non lorem erat.
+    </span>{' '}
+                    Ut augue massa, aliquam in bibendum sed, euismod vitae magna. Nulla sit amet
+                    sodales augue. Curabitur in nulla in magna luctus porta et sit amet dolor.
+                    Pellentesque a magna enim. Pellentesque malesuada egestas urna, et pulvinar
+                    lorem viverra suscipit. Duis sit amet mauris ante. Fusce at ante nunc.
+                    Maecenas ut leo eu erat porta fermentum.
+                </p>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <p  ref={ref2} >11</p>
+                <AnimatePresence>
+                    {isOpen ? (
+                        <motion.div
+                            initial={{opacity: 0}}
+                            animate={{opacity: 1}}
+                            exit={{opacity: 0}}
+                            style={{position: 'relative', zIndex: 99999}}
+                        >
+                            <Mask
+                                sizes={sizes}
+                                styles={{maskWrapper: base => ({...base, zIndex: 99999})}}
+                                onClick={() => {
+                                    setIsOpen(false)
+                                }}
+                            />
+                            <Popover sizes={sizes} className={"pop-up"}>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                                    volutpat quam eu mauris euismod imperdiet.
+                                </p>
+                                <button>Next</button>
+                            </Popover>
+                        </motion.div>
+                    ) : null}
+                </AnimatePresence>
+            </div>
         </div>
     );
 }
